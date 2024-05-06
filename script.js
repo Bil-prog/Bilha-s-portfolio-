a7ff10c425cc82a51ce4e1a8627fc2ffa0019cca
Bilha Mwengi <bilhamwengi34@gmail.com>
2024-05-06 22:17:20 +0300

    fifth commit

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2500,
+    reset: true
+})
+sr.reveal('.logo', {delay:200, origin: 'left'})
+sr.reveal('.nav-links', {delay:350, origin: 'top'})
+sr.reveal('.hamburger-icon', {delay:350, origin: 'right'})
+sr.reveal('.menu-links', {delay:350, origin: 'right'})
+sr.reveal('.profilepic', {delay:450, origin: 'left'})
+sr.reveal('.section_text', {delay:450, origin: 'right'})
+
+sr.reveal('.abt-pic', {delay:450, origin: 'left'})
+sr.reveal('.one', {delay:450, origin: 'right'})
+sr.reveal('.two', {delay:480, origin: 'right'})
+sr.reveal('.text-container', {delay:540, origin: 'right'})
